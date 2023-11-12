ed1de42f569b07e917e4e8fabc2c34b214e8e806
ina-verena <144120729+ina-verena@users.noreply.github.com>
2023-11-12 11:57:14 +0100

    endpoints

--- README.md
+++ README.md
@@ -16,3 +16,14 @@ Boolean werden als 0(false) und 1(true) gespeichert
 
 ***
 
+### REST erstellen
+- Node.js Projekt erstellen `npm init -y`  
+- Node.js Packete instalieren `npm i express sqlite3`  
+- Nodemon instalieren `npm i -g nodemon` (Tool um Server nicht immer neu starten zu müssen)
+- Datei erstellen z.B. `server.js`  
+- Server starten `node server.js` oder `nodemon server.js`
+
+***
+
+### REST Testen
+Installiere dieses Plugin: Thunder Client
--- server.js
+++ server.js
@@ -3,4 +3,20 @@ const app = express();
 
 const HTTP_PORT = 8000;
 
-app.listen(HTTP_PORT);
+// Server starten mit Console: node server.js (Name dieser Datei)
+// Mit nodemon werden Änderungen aktualisiert: nodemon server.js
+app.listen(HTTP_PORT, () =>{
+    console.log(`Server running on port ${HTTP_PORT}`);
+});
+
+
+// Endpunkte definieren
+// req = Das was vom Client kommt
+// res = Die Antwort vom Server
+app.get("/api", (req, res)=>{
+    res.json({message: "OK"})
+});
+// app.get();
+// app.post();
+// app.patch();
+// app.delete();
